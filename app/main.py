5a6955b55fef95c608ee55c72e2a65b31ac04d76
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-15 22:32:52 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import datetime
+
+
+class OutdatedVaccineError(Exception):
+    pass
+
+
+class NotVaccinatedError(Exception):
+    pass
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> None:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError
+        actuel_data = visitor["vaccine"].get["expiration_date"]
